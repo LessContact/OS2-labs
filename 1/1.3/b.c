70905ebff91b76f9d5784e3c681f19c916137616
LessContact <lon@no.com>
2024-10-23 14:53:03 +0700

    i lament this existance 2

--- 1/1.3/b.c
+++ 1/1.3/b.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
 typedef struct test {
     int value_int;
     char* ptr_char;
@@ -12,19 +11,14 @@ typedef struct test {
 
 void* my_thread(void *arg) {
     struct test* node = (struct test*)arg;
-    int a = 5;
     printf("value_int value: %d\n", node->value_int);
     printf("ptr_char ptr: %s\n", node->ptr_char);
 
-    sleep(7);
-    printf("value stack %d", a);
-
-    pthread_exit(NULL);
-
+    return NULL:
 }
 // cansletion pointer
 int main() {
-    test* node;
+    test* node = malloc(sizeof(test));
     node->value_int = 42;
     node->ptr_char = "hello";
     pthread_attr_t attr;
@@ -35,6 +29,7 @@ int main() {
     if(err) {
         fprintf(stderr, "main: pthread_attr_setdetachstate() failed %s\n", strerror(err));
         pthread_attr_destroy(&attr);
+        free(node);
         return EXIT_FAILURE;
     }
 
@@ -42,14 +37,13 @@ int main() {
     err = pthread_create(&thread, &attr, my_thread, (void *)node);
     if (err) {
         fprintf(stderr, "main: pthread_create() failed: %s\n", strerror(err));
+        free(node);
         return EXIT_FAILURE;
     }
 
-    sleep(3);
+    sleep(10);
     pthread_attr_destroy(&attr);
 
-    //free(node);
+    free(node);
     return EXIT_SUCCESS;
 }
-
-//// gdb
