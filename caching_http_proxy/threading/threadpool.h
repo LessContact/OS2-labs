b71db1f48ecd7eb963bc222540482939c0f1f112
LessContact <lon@no.com>
2025-01-21 16:25:19 +0700

    before major changes to process_request

--- caching_http_proxy/threading/threadpool.h
+++ caching_http_proxy/threading/threadpool.h
@@ -6,14 +6,37 @@
 #include <poll.h>
 
 #include "../caching/httpcache.h"
+#include "../proxy/proxy.h"
 
 #define MAX_WORKER_THREADS 8
 #define MAX_CLIENTS_PER_THREAD 64
+
+typedef enum {
+    STATE_READ_REQUEST,
+    STATE_CONNECT_REMOTE,
+    STATE_SEND_REQUEST,
+    STATE_READ_RESPONSE_HEADERS,
+    STATE_PROCESS_RESPONSE,
+    STATE_FORWARD_BODY,
+    STATE_CLEANUP
+} request_state_t;
 
 typedef struct _con_ctx {
     int sock_fd;
     http_cache_t *cache;
     //int is_forwarded;
+
+    // New fields for state management
+    request_state_t state;
+    int remote_sock_fd;
+    char buffer[BUFFER_SIZE];
+    size_t buflen;
+    size_t bytes_processed;
+    response_t response;
+    cache_entry_t *cache_entry;
+    size_t content_length;
+    size_t total_received;
+    int do_cache;
 } connection_ctx_t;
 
 typedef struct _worker_data {
